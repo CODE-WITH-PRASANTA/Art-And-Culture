dcd1db898cb84f86289ecf09eec7a6aa9c029192
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-09-22 11:44:45 +0530

    Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,6 @@ import Terms from "./Components/Termandcondition/Termandcondition";
 import PoojaDetails from "./Pages/PoojaDetails/PoojaDetails";
 import ShopDetails from "./Pages/ShopDetails/ShopDetails";
 import LoginForm from "./Components/LoginForm/LoginForm";
-
 import Prints from "./Pages/Prints/Prints";
 import MainLayout from "./Layout/MainLayout/MainLayout";
 import Address from "./Components/Address/Address";
@@ -33,15 +32,14 @@ import Wishlist from "./Components/Whislist/Wishlist";
 import ShopBestseller from "./Pages/ShopBestseller/ShopBestseller";
 import ExploreCollections from "./Pages/ExploreCollections/ExploreCollections";
 
-
 function App() {
   return (
     <>
       <Navbar />
       <main style={{ minHeight: "80vh" }}>
         <Routes>
-          <Route path="/" element={<Home />} />
           {/* General Routes */}
+          <Route path="/" element={<Home />} />
           <Route path="/shop" element={<ShopSec />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
@@ -58,49 +56,26 @@ function App() {
           <Route path="/trackorder" element={<TrackOrder />} />
           <Route path="/faq" element={<Faq />} />
           <Route path="/poojadetails" element={<PoojaDetails />} />
-         <Route path="/shopdetails" element={<ShopDetails />} />
-          <Route path="/poojamain" element={<PoojaEssentialsMain />} />
-         <Route path="/shopdetails/:id" element={<ShopDetails />} />
-         <Route path="/poojamain"element={<PoojaEssentials/>}/>
-          <Route path="/wood" element={<Prints />} />   
-          <Route path="wishlist" element={<Wishlist/>}/>
-          <Route path="/LoginForm" element={<LoginForm/>}/>
-
-          {/* ================= NESTED ACCOUNT ROUTING BLOCK ================= */}
-         <Route path="/account" element={<MainLayout />}>
-
-  <Route index element={<div>Overview Page</div>} />
-
-  <Route
-    path="address"
-    element={<Address />}
-  />
-
-  <Route
-    path="overview"
-    element={<OverView />}
-  />
-   <Route path="orders" element={<Order />} />
-
-  <Route
-    path="details"
-    element={<AccountDetails />}
-  />
-
-  <Route
-    path="contact"
-    element={<ContactUs />} 
-  />
-
-</Route>
-{/* 2. ADDED SHOPBESTSELLER ROUTE */}
+          <Route path="/poojamain" element={<PoojaEssentials />} />
+          <Route path="/shopdetails" element={<ShopDetails />} />
+          <Route path="/shopdetails/:id" element={<ShopDetails />} />
+          <Route path="/wood" element={<Prints />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/loginform" element={<LoginForm />} />
           <Route path="/shopbestseller" element={<ShopBestseller />} />
-          <Route path="/explorecollections" element={<ExploreCollections/>}/>
-          {/* Layout Setup */}
-         
+          <Route path="/explorecollections" element={<ExploreCollections />} />
+
+          {/* Nested Account Routing Block */}
+          <Route path="/account" element={<MainLayout />}>
+            <Route index element={<OverView />} />
+            <Route path="overview" element={<OverView />} />
+            <Route path="address" element={<Address />} />
+            <Route path="orders" element={<Order />} />
+            <Route path="details" element={<AccountDetails />} />
+            <Route path="contact" element={<ContactUs />} />
+          </Route>
         </Routes>
       </main>
-
       <Footer />
       <FloatingIcons />
     </>
